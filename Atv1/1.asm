6a3710b21626ad32383b31ce96984208ae2f6246
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2024-05-12 18:54:32 -0300

    Questão 1 finalizada

--- Atv1/1.asm
+++ Atv1/1.asm
@@ -1,9 +1,5 @@
     .data
 
-    a: .word 0
-    b: .word 0
-    c: .word 0
-
     .text
 
 main: 
@@ -16,21 +12,33 @@ main:
     mv s2, a0 # s2 = c
     j forma_triangulo
 forma_triangulo:
-    # (a + b > c) and (a + c > b) and (b + c > a): 
+    # (a + b > c) e (a + c > b) e (b + c > a): 
     add s3, s0, s1 # s3 = a + b
     add s4, s0, s3 # s4 = a + c
     add s5, s1, s2 # s5 = b + c
 
-    bge s3, s2, primeira_condicao
-        j fim
+    bge s3, s2, primeira_condicao # a + b >= c
+        j nao_forma 
 primeira_condicao:
-    bge s4, s1, segunda_condicao
-        j fim
-segunda_condicao:
-    bge s5, s0, ok
-        j fim
-ok:
+    beq s3, s2, nao_forma # a + b = c // nao forma triangulo
+        j segunda_condicao 
+segunda_condicao: # a + b > c
+    bge s4, s1, terceira_condicao
+        j nao_forma
+terceira_condicao: 
+     beq s4, s1, nao_forma # a + c = b // nao forma triangulo
+        j quarta_condicao
+quarta_condicao:
+     bge s5, s0, quinta_condicao # b + c >= a
+        j nao_forma
+quinta_condicao:
+     beq s5, s0, nao_forma # b + c = a // nao forma triangulo
+        j forma
+forma:
     li a0, 1
+    j fim
+nao_forma:
+    li a0, 0
     j fim
 fim:
     nop 
