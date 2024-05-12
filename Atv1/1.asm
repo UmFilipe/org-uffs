a67cb7338869141dca932bfdb89ec9d03d952aaa
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2024-05-12 18:06:33 -0300

    Questão 1

--- Atv1/1.asm
+++ Atv1/1.asm
@@ -7,19 +7,30 @@
     .text
 
 main: 
-    li a10, 4
+    li a7, 5
     ecall
-    add s0, a0, zero
+    mv s0, a0 # s0 = a0
     ecall
-    add s1, a1, zero
+    mv s1, a0 # s1 = b
     ecall
-    add s2, a2, zero
-    ecall
-    bge s0, s1, a_maior_que_b
-    	j a_menor_que_b
-a_maior_que_b:
-    bge s0, s2, a_maior
-a_menor_que_b:
-    bge s1, s2, b_maior
+    mv s2, a0 # s2 = c
+    j forma_triangulo
+forma_triangulo:
+    # (a + b > c) and (a + c > b) and (b + c > a): 
+    add s3, s0, s1 # s3 = a + b
+    add s4, s0, s3 # s4 = a + c
+    add s5, s1, s2 # s5 = b + c
+
+    bge s3, s2, primeira_condicao
+        j fim
+primeira_condicao:
+    bge s4, s1, segunda_condicao
+        j fim
+segunda_condicao:
+    bge s5, s0, ok
+        j fim
+ok:
+    li a0, 1
+    j fim
 fim:
-    	nop 
+    nop 
